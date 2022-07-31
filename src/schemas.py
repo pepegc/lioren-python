77e0b8195f8cf1ec727702d5c4ac773924320f2e
pepegc <jpgarcia90@gmail.com>
2022-07-31 12:01:31 -0400

    types

---
+++ src/schemas.py
@@ -0,0 +1,74 @@
+from enum import Enum
+from pydantic import BaseModel, EmailStr, Optional
+from datetime import date
+
+
+Telefono = conint(ge=100000000, le=999999999)
+RUT = str  # TODO
+
+TipoDocumento = constr(min_length=2, max_length=3, strip_whitespace=True)
+RazonSocial = constr(min_length=5, max_length=100, strip_whitespace=True)
+Giro = constr(min_length=5, max_length=40, strip_whitespace=True)
+Localidad = constr(min_length=3, max_length=20, strip_whitespace=True)
+Direccion = constr(min_length=5, max_length=50, strip_whitespace=True)
+Nombre = constr(min_length=5, max_length=80, strip_whitespace=True)
+
+
+class ExpectsDTE(str, Enum):
+    xml = "xml"
+    pdf = "pdf"
+    _all = "all"
+
+
+class EmisorDTE(BaseModel):
+    tipodoc: TipoDocumento
+    fecha: date
+    actividad: Optional[int]
+    sucursal: Optional[int]
+    email: Optional[EmailStr]
+    telefono: Optional[Telefono]
+    tipodespacho: Optional[int]
+    tipotraslado: Optional[int]
+
+
+class ReceptorDTE(BaseModel):
+    rut: RUT
+    rs: RazonSocial
+    giro: Giro
+    comuna: int
+    ciudad: int
+    localidad: Optional[Localidad]
+    direccion: str
+    email: Optional[EmailStr]
+    telefono: Optional[Telefono]
+    nombresolicitante: Opcional[Nombre]
+    rutsolicitante: Optional[RUT]
+
+
+class DetalleDTE(BaseModel):
+    codigo: Optional[constr(min_length=3, max_length=128, strip_whitespace=True)]
+    nombre: Nombre
+    cantidad: confloat(ge=0.000001, le=999999999)
+    unidad: Optional[constr(min_length=1, max_length=4, strip_whitespace=True)]
+    precio: confloat(ge=0, le=999999999)
+    impuestoadicional: Optional[conint(ge=10, le=999)]
+    montoimpuesto: Optional[conint(ge=10, le=999999999)]
+    descuento: Optional[conint(ge=1, le=100)]
+    descripcion: Optional[constr(max_length=1000, strip_whitespace=True)]
+    exento: bool
+    bodega: Optional[int]
+
+
+class PagoDTE(BaseModel):
+    fecha: date
+    mediopago: int
+    glosa: Optional[constr(max_length=40, strip_whitespace=True)]
+    cobrar: bool
+
+
+class Referencias(BaseModel):
+    fecha: date
+    tipodoc: TipoDocumento
+    folio: constr(min_length=1, max_length=18, strip_whitespace=True)
+    razon: int
+    glosa: constr(min_length=5, max_length=80, strip_whitespace=True)
