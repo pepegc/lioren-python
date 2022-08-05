12d3b630887fa529c022dfda3c3fdede117a03a1
pepegc <jpgarcia90@gmail.com>
2022-08-04 20:26:41 -0400

    use httpx instead of requests

--- src/core.py
+++ src/core.py
@@ -1,5 +1,5 @@
 import json
-import requests
+import httpx
 from typing import Optional
 
 from lioren.config import LIOREN_BASE_URL as BASE_URL
@@ -7,26 +7,26 @@ from lioren.config import LIOREN_TOKEN as TOKEN
 
 
 def _get(uri: str, params: Optional[object]):
-    response = requests.get(BASE_URL + uri, headers=__headers(), params=params)
+    response = httpx.get(BASE_URL + uri, headers=__headers(), params=params)
     return response.json()
 
 
 def _post(uri: str, data: Optional[object], params: Optional[object]):
-    response = requests.post(
+    response = httpx.post(
         BASE_URL + uri, headers=__headers(), data=json.dumps(data), params=params
     )
     return response.json()
 
 
 def _put(uri: str, data: Optional[object], params: Optional[object]):
-    response = requests.put(
+    response = httpx.put(
         BASE_URL + uri, headers=__headers(), data=json.dumps(data), params=params
     )
     return response.json()
 
 
 def _delete(uri: str, params: Optional[object]):
-    response = requests.delete(BASE_URL + uri, headers=__headers(), params=params)
+    response = httpx.delete(BASE_URL + uri, headers=__headers(), params=params)
     return response.json()
 
 
