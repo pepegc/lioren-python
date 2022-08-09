2b94229e6639c48f269806f2a8ad228879450ee5
pepegc <jpgarcia90@gmail.com>
2022-08-09 12:59:15 -0400

    better typing hints

--- lioren/core.py
+++ lioren/core.py
@@ -1,36 +1,40 @@
 import json
 import httpx
-from typing import Optional
+from typing import Any, Optional
 
 from lioren.config import LIOREN_BASE_URL as BASE_URL
 from lioren.config import LIOREN_TOKEN as TOKEN
 
+RequestParams = dict[str, str]
+RequestData = dict[str, Any]
+APIResponse = dict[str, Any]
 
-def _get(uri: str, params: Optional[object]):
+
+def _get(uri: str, params: Optional[RequestParams]) -> APIResponse:
     response = httpx.get(BASE_URL + uri, headers=__headers(), params=params)
     return response.json()
 
 
-def _post(uri: str, data: Optional[object], params: Optional[object]):
+def _post(uri: str, data: Optional[RequestData], params: Optional[RequestParams]) -> APIResponse:
     response = httpx.post(
         BASE_URL + uri, headers=__headers(), data=json.dumps(data), params=params
     )
     return response.json()
 
 
-def _put(uri: str, data: Optional[object], params: Optional[object]):
+def _put(uri: str, data: Optional[RequestData], params: Optional[RequestParams]) -> APIResponse:
     response = httpx.put(
         BASE_URL + uri, headers=__headers(), data=json.dumps(data), params=params
     )
     return response.json()
 
 
-def _delete(uri: str, params: Optional[object]):
+def _delete(uri: str, params: Optional[RequestParams]) -> APIResponse:
     response = httpx.delete(BASE_URL + uri, headers=__headers(), params=params)
     return response.json()
 
 
-def __headers():
+def __headers() -> dict[str, str]:
     return {
         "Accept": "application/json",
         "Authorization": f"Bearer {TOKEN}",
